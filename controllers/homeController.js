34a4d9c1777465aad6957634d29158de98ec88a6
digital-programmer <mohandibya123@gmail.com>
2021-10-04 01:09:30 +0530

    Add home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,7 @@
 module.exports.home = function (req, res) {
-  return res.end("<h1>Exxpress is up for codial</h1>");
+  return res.render("home", {
+    title: "Cordial",
+  });
 };
 
 module.exports.about = function (req, res) {
