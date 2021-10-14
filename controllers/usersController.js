006f3ba0d50c2e4b32a576022c49742989331eeb
digital-programmer <mohandibya123@gmail.com>
2021-10-14 10:28:11 +0530

    Set up sign in and sign out manual authentication

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,24 @@
 const User = require('../models/user')
 
+// show user profile if signed in
 module.exports.profile = function (req, res) {
-  res.render("user_profile", {
-    title: "Profile",
-  });
+  // check if user is in cookies
+  if(req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function(err,user){
+      if(user){
+        return res.render("user_profile", {
+          title: "User Profile",
+          user: user,
+        });
+      }
+      return res.redirect("/users/sign-in");
+    });
+  } else {
+    res.redirect("/users/sign-in");
+  }
 };
 
+// show user timeline
 module.exports.timeline = function (req, res) {
   res.end("<h1>User Timeline</h1>");
 };
@@ -55,5 +68,40 @@ module.exports.create = function (req, res) {
 
 // create user session
 module.exports.createSession = function (req, res) {
-  // Later
+  // steps to authenticate
+
+  // find the user
+  User.findOne({email: req.body.email}, function(err,user) {
+    if(err) {
+      console.log("Error in finding user in signing in");
+      return;
+    }
+  // handle user found
+    if(user){
+      // handle incorrect password
+      if(user.password != req.body.password) {
+        return res.redirect("back");
+      }
+      // handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } 
+    // handle user nt found
+    else {
+      return res.redirect("back");
+    }
+
+  });
+
+
+
+
+
+  // if not found
+}
+
+// sign-out user
+module.exports.signOut = function (req, res) {
+  res.clearCookie('user_id');
+  return res.redirect("back");
 }
